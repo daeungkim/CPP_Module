cc054d420f2fa5cefaa21ccdb74253543ff68a05
stevenkim <11882@naver.com>
2020-10-02 00:43:33 +0900

    cpp06 ex02

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seunkim <seunkim@student.42.fr>            +#+  +:+       +#+        */
+/*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 22:40:57 by seunkim           #+#    #+#             */
-/*   Updated: 2020/10/01 22:52:00 by seunkim          ###   ########.fr       */
+/*   Updated: 2020/10/02 00:37:04 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,48 @@
 
 Base* generate(void)
 {
-	
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(0, 2);
+
+    int num = dis(gen);
+    
+    if (num == 0)
+        return ((new A));
+    else if (num == 1)
+        return ((new B));
+    else if (num == 2)
+        return ((new C));
+    return (nullptr);
 }
 
-void identify_from_pointer(Base* p);
+void identify_from_pointer(Base* p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+}
 
-void identify_from_reference(Base& p);
+void identify_from_reference(Base& p)
+{
+    identify_from_pointer(&p);
+}
 
 int main(void)
 {
+    Base *base = generate();
+    Base &ref = *base;
 
+    identify_from_pointer(base);
+    identify_from_reference(ref);
+
+    Base *base2 = generate();
+    Base &ref2 = *base2;
+
+    identify_from_pointer(base2);
+    identify_from_reference(ref2);
+    
 }
