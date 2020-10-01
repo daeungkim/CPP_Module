01a09a149e1a143ec8740a6b0d7efa8d22f8d7bc
Seungwoo Kim <seunkim@c2r8s1.42seoul.kr>
2020-10-01 22:52:31 +0900

    cpp06 ex02 save

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunkim <seunkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/01 22:40:57 by seunkim           #+#    #+#             */
+/*   Updated: 2020/10/01 22:52:00 by seunkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base* generate(void)
+{
+	
+}
+
+void identify_from_pointer(Base* p);
+
+void identify_from_reference(Base& p);
+
+int main(void)
+{
+
+}
